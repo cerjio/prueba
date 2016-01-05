9bd1888374b62a89ac519e8a55d94edd970a0497
cerjio <cerjio@gmail.com>
2016-01-05 11:22:52 -0600

    Initial Commit

---
+++ pruebaGit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit2
+//
+//  Created by cerjio on 05/01/16.
+//  Copyright © 2016 cerjio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
